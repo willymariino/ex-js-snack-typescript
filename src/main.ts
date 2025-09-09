f3a6e3eb8fbc3b1b8e1b9c2c37a3c446fde71333
willymariino <wg.mariino@gmail.com>
2025-09-09 04:43:12 +0200

    snake 3

--- src/main.ts
+++ src/main.ts
@@ -46,15 +46,38 @@ anniDiServizio (array di numeri, es. [2014, 2015, 2017, 2018])
 */
 
 type Dipendente = {
-  nome: string,
-  cognome: string,
-  annoNascita: number,
-  sesso: "m" | "f",
-  anniDiServizio: (number)[],
+  nome: string;
+  cognome: string;
+  annoNascita: number;
+  sesso: "m" | "f";
+  anniDiServizio: (number)[];
 
 }
 
 
 /*
+Estendiamo Dipendente per definire due ruoli specifici all'interno dell'azienda:
 
+1) Developer
+livelloEsperienza → Il livello di esperienza del developer (le scelte possibili sono solo “Junior”, “Mid” o “Senior”).
+linguaggi → Un array contenente i linguaggi di programmazione utilizzati dal developer in azienda (opzionale, perché i neo assunti non hanno ancora dei linguaggi assegnati).
+certificazioni → Un array di stringhe contenente certificazioni tecniche ottenute dal developer (può essere vuoto).
+2) ProjectManager
+teamSize → Il numero di persone nel team gestito dal Project Manager (può essere null se non ha ancora un team assegnato).
+budgetGestito → Il totale del budget annuale gestito dal PM (opzionale).
+stakeholderPrincipali → Un array di stringhe con i nomi dei principali stakeholder con cui il PM collabora (può essere vuoto).
 */
+
+type Developer = Dipendente & {
+
+  livelloEsperienza: "Junior" | "Mid" | "Senior";
+  linguaggi?: string[];
+  certificazioni: string[]
+
+}
+
+type ProjectManager = Dipendente & {
+  teamSize: (string | null)[]
+  budgetGestito?: number
+  stakeHolder: (string | null)[]
+}
